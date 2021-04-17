b523cec1db6d876da57ecdd58779d28f29bf2a43
Ein_Green <mbananola2002@gmail.com>
2021-04-16 21:34:12 -0600

    added a form to GET an activity depending on the number of participants

--- js/app.js
+++ js/app.js
@@ -17,3 +17,23 @@ let randomActivityButton = document.getElementById("randomActivity");
 randomActivityButton.addEventListener("click", getRandomActivity);
 
 // dropdown menu function: 
+function yourActivitySuccess(res) {
+    let participantDataAct = res.data.activity;
+    let actContainer = document.getElementById("participantActivityContainer");
+    actContainer.innerText = `Your Activity: ${participantDataAct}`;
+}
+function yourActivityFailure(err) {
+    console.log(err);
+    let errorContainer = document.getElementById("participantActivityContainer");
+    errorContainer.innerText = `Nope, sorry mate`;
+}
+function participantActivity(eventDetails) {
+    let numparticipants = document.getElementById("selector").value;
+    axios.request({
+        method: "GET",
+        url: `http://www.boredapi.com/api/activity?participants=${numparticipants}`,
+    }).then(yourActivitySuccess).catch(yourActivityFailure);
+}
+
+let submitNum = document.getElementById("submitNumButton");
+submitNum.addEventListener("click", participantActivity);
