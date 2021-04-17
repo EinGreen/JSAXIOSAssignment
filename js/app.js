fca77c55b051fddc3e92fce37b9292eee188fbd6
Ein_Green <mbananola2002@gmail.com>
2021-04-16 22:02:49 -0600

    removed/added comments, realised I forgot to comment and push to gitHub so hopefully this makes it there

--- js/app.js
+++ js/app.js
@@ -2,11 +2,16 @@
 function randomActivitySuccess(res) {
     let randomData = res.data;
     let actContainer = document.getElementById("randomActContainer");
+    // Note: the variable above is targeting a long Id
     actContainer.innerText = `${randomData.activity}`;
 }
+// The function above will only happen if the getRandomActivity function actually works when it's clicked
 function randomActivityFailure(err) {
     console.log(err);
+    let errorContainer = document.getElementById("randomActContainer");
+    errorContainer.innerText = `Fail`;
 }
+// The function above will get called when getRandomActivity fails
 function getRandomActivity(eventDetails) {
     axios.request({
         method: "GET",
@@ -15,11 +20,13 @@ function getRandomActivity(eventDetails) {
 }
 let randomActivityButton = document.getElementById("randomActivity");
 randomActivityButton.addEventListener("click", getRandomActivity);
+// Easiest one. Just have to GET an activity from the API and then inject the data to the HTML file
 
 // dropdown menu function: 
 function yourActivitySuccess(res) {
     let participantDataAct = res.data.activity;
     let actContainer = document.getElementById("participantActivityContainer");
+    // Note: disregard what I said about "randomActContainer" being long
     actContainer.innerText = `Your Activity: ${participantDataAct}`;
 }
 function yourActivityFailure(err) {
@@ -29,11 +36,12 @@ function yourActivityFailure(err) {
 }
 function participantActivity(eventDetails) {
     let numparticipants = document.getElementById("selector").value;
+    // Note: had to get the value for the selection made by the user from the html
     axios.request({
         method: "GET",
         url: `http://www.boredapi.com/api/activity?participants=${numparticipants}`,
+        // Inserted value of user's option to the url via veriable
     }).then(yourActivitySuccess).catch(yourActivityFailure);
 }
-
 let submitNum = document.getElementById("submitNumButton");
 submitNum.addEventListener("click", participantActivity);
